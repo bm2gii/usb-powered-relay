290cc5c4af72c7ae5cbf0f0f60247817f93cd6cf
Tse-lei Wu <tselei.wu@gmail.com>
2022-02-21 17:18:45 +0800

    add parameter for 4 port

--- usbser2relay.ps1
+++ usbser2relay.ps1
@@ -2,16 +2,65 @@
  # USB relay module: LCUS-1 (http://images.100y.com.tw/pdf_file/57-LCUS-1.pdf)
  #>
 
-[System.IO.Ports.SerialPort]::getportnames()
+param ([Int] $usb_port=0, [String] $com_port= "COM44", [int] $cmd=0)
 
-[Byte[]] $cmdON = 0xA0,0x01,0x01,0xA2
-[Byte[]] $cmdOFF = 0xA0,0x01,0x00,0xA1
+#echo "$com_port to usb $usb_port"
 
-$port= new-Object System.IO.Ports.SerialPort COM7,9600,None,8,one
-$port.open()
+if ((0 -eq $usb_port) -or (0 -eq $cmd))
+{
+	
+	$scriptName = $myInvocation.myCommand.name
+	echo "$scriptName usb_port com_port cmd"
+	echo "1:on_off"
+	echo "2:on"
+	echo "3:off"
+	echo "4:offon"
+	echo ""
+	echo ""
+	
+	[System.IO.Ports.SerialPort]::getportnames()
 
-$port.Write($cmdON, 0, $cmdON.Count)
-Start-Sleep -s 1.5
-$port.Write($cmdOFF, 0, $cmdOFF.Count)
+}
+else
+{
+	#echo $usb_port
+	$ON_CK = 161 + $usb_port
+	$OFF_CK = 160 + $usb_port
+	#echo $ON_CK
+	[Byte[]] $cmdON = 0xA0,$usb_port,0x01,$ON_CK
+	[Byte[]] $cmdOFF = 0xA0,$usb_port,0x00,$OFF_CK
 
-$port.Close()
+	#echo "usb: $usb_port"
+	#echo "OON: $cmdON"
+	#echo "OFF: $cmdOFF"
+
+	$port= new-Object System.IO.Ports.SerialPort $com_port,9600,None,8,one
+	$port.open()
+	
+	
+	if (1 -eq $cmd)
+	{
+		$port.Write($cmdON, 0, $cmdON.Count)
+		Start-Sleep -s 1.5
+		$port.Write($cmdOFF, 0, $cmdOFF.Count)
+	}
+
+	if (4 -eq $cmd) 
+	{
+		$port.Write($cmdOFF, 0, $cmdOFF.Count)
+		Start-Sleep -s 1.5
+		$port.Write($cmdON, 0, $cmdON.Count)
+	}
+	
+	if (2 -eq $cmd)
+	{
+		$port.Write($cmdON, 0, $cmdON.Count)
+	}
+	
+	if (3 -eq $cmd)
+	{
+		$port.Write($cmdOFF, 0, $cmdOFF.Count)
+	}
+
+	$port.Close()
+}
